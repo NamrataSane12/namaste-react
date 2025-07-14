ce051cee30eadc3407f863e9d688864eef616520
Your Name <you@example.com>
2025-07-15 00:15:44 +0400

    initial master

---
+++ App.js
@@ -0,0 +1,35 @@
+/*HTMLExample
+<div id="parent">
+  <div id="child">
+    <h1>I'm H1 Tag</h1>
+    <h2>I'm H2 Tag</h2>
+  </div>
+
+  <div id="child2">
+    <h1>I'm H1 Tag</h1>
+    <h2>I'm H2 Tag</h2>
+  </div>
+</div>
+
+
+*/
+
+const parent = React.createElement("div", { id: "parent" }, 
+    [
+        React.createElement("div", { id: "child" },
+        [
+            React.createElement("h1", {}, "I'm H1 Tag"),
+            React.createElement("h2", {}, "I'm H2 Tag")
+        ]),
+
+        React.createElement("div", { id: "child2" },
+        [
+            React.createElement("h1", {}, "I'm H1 Tag"),
+            React.createElement("h2", {}, "I'm H2 Tag")
+        ])
+    ]);
+
+
+const root= ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
